83aa73cfc4b54a1ccbd86e66cec20305e191807e
Nenad Vasic <nenadbg@gmail.com>
2022-02-11 18:59:51 +0100

    Delete duplicated line

--- src/main/kotlin/com/nenadvasic/orderbook/Application.kt
+++ src/main/kotlin/com/nenadvasic/orderbook/Application.kt
@@ -11,6 +11,5 @@ fun main(args: Array<String>): Unit =
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureRouting()
-    configureRouting()
     configureSockets()
 }
